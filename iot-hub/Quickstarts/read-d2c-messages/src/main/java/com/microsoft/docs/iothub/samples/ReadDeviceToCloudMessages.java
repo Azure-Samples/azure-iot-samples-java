ce8f7c2fc9f91e3e1f50640438b54ad49c48de0c
Srikanta <srnagar@microsoft.com>
2020-05-21 13:37:24 -0700

    Fix message formatting and read latest events only

--- iot-hub/Quickstarts/read-d2c-messages/src/main/java/com/microsoft/docs/iothub/samples/ReadDeviceToCloudMessages.java
+++ iot-hub/Quickstarts/read-d2c-messages/src/main/java/com/microsoft/docs/iothub/samples/ReadDeviceToCloudMessages.java
@@ -74,21 +74,21 @@ public class ReadDeviceToCloudMessages {
   }
 
   /**
-   * This method receives events from all partitions asynchronously starting from the earliest available event in
+   * This method receives events from all partitions asynchronously starting from the newly available events in
    * each partition.
    *
    * @param eventHubConsumerAsyncClient The {@link EventHubConsumerAsyncClient}.
    */
   private static void receiveFromAllPartitions(EventHubConsumerAsyncClient eventHubConsumerAsyncClient) {
-    System.out.println("Receiving events from all partitions");
 
     eventHubConsumerAsyncClient
-        .receive(true) // set this to false to read only the newly available events
+        .receive(false) // set this to false to read only the newly available events
         .subscribe(partitionEvent -> {
-          System.out.printf("Telemetry received from partition %s:%n%s",
+          System.out.printf("%nTelemetry received from partition %s:%n%s",
               partitionEvent.getPartitionContext().getPartitionId(), partitionEvent.getData().getBodyAsString());
-          System.out.printf("%nApplication properties (set by device):%n" + partitionEvent.getData().getProperties());
-          System.out.printf("%nSystem properties (set by IoT Hub):%n" + partitionEvent.getData().getSystemProperties());
+          System.out.printf("%nApplication properties (set by device):%n%s", partitionEvent.getData().getProperties());
+          System.out.printf("%nSystem properties (set by IoT Hub):%n%s",
+              partitionEvent.getData().getSystemProperties());
         }, ex -> {
           System.out.println("Error receiving events " + ex);
         }, () -> {
@@ -98,24 +98,24 @@ public class ReadDeviceToCloudMessages {
 
   /**
    * This method queries all available partitions in the Event Hub and picks a single partition to receive
-   * events asynchronously starting from the earliest available event in that partition.
+   * events asynchronously starting from the newly available event in that partition.
    *
    * @param eventHubConsumerAsyncClient The {@link EventHubConsumerAsyncClient}.
    */
   private static void receiveFromSinglePartition(EventHubConsumerAsyncClient eventHubConsumerAsyncClient) {
-    System.out.println("Receiving events from a single partition");
     eventHubConsumerAsyncClient
         .getPartitionIds() // get all available partitions
         .take(1) // pick a single partition
         .flatMap(partitionId -> {
           System.out.println("Receiving events from partition id " + partitionId);
           return eventHubConsumerAsyncClient
-              .receiveFromPartition(partitionId, EventPosition.earliest());
+              .receiveFromPartition(partitionId, EventPosition.latest());
         }).subscribe(partitionEvent -> {
-          System.out.printf("Telemetry received from partition %s: %s %n",
+          System.out.printf("%nTelemetry received from partition %s:%n%s",
               partitionEvent.getPartitionContext().getPartitionId(), partitionEvent.getData().getBodyAsString());
-          System.out.println("Application properties (set by device): " + partitionEvent.getData().getProperties());
-          System.out.println("System properties (set by IoT Hub): " + partitionEvent.getData().getSystemProperties());
+          System.out.printf("%nApplication properties (set by device):%n%s", partitionEvent.getData().getProperties());
+          System.out.printf("%nSystem properties (set by IoT Hub):%n%s",
+              partitionEvent.getData().getSystemProperties());
         }, ex -> {
           System.out.println("Error receiving events " + ex);
         }, () -> {
@@ -126,28 +126,28 @@ public class ReadDeviceToCloudMessages {
 
   /**
    * This method queries all available partitions in the Event Hub and picks a single partition to receive
-   * events asynchronously in batches of 100 events, starting from the earliest available event in that partition.
+   * events asynchronously in batches of 100 events, starting from the newly available event in that partition.
    *
    * @param eventHubConsumerAsyncClient The {@link EventHubConsumerAsyncClient}.
    */
   private static void receiveFromSinglePartitionInBatches(EventHubConsumerAsyncClient eventHubConsumerAsyncClient) {
     int batchSize = 100;
-    System.out.printf("Receiving events in batches of %s events from a single partition %n", batchSize);
     eventHubConsumerAsyncClient
         .getPartitionIds()
         .take(1)
         .flatMap(partitionId -> {
           System.out.println("Receiving events from partition id " + partitionId);
           return eventHubConsumerAsyncClient
-              .receiveFromPartition(partitionId, EventPosition.earliest());
+              .receiveFromPartition(partitionId, EventPosition.latest());
         }).window(batchSize) // batch the events
         .subscribe(partitionEvents -> {
               partitionEvents.toIterable().forEach(partitionEvent -> {
-                System.out.printf("Telemetry received from partition %s: %s %n",
+                System.out.printf("%nTelemetry received from partition %s:%n%s",
                     partitionEvent.getPartitionContext().getPartitionId(), partitionEvent.getData().getBodyAsString());
-                System.out.println("Application properties (set by device): " + partitionEvent.getData().getProperties());
-                System.out
-                    .println("System properties (set by IoT Hub): " + partitionEvent.getData().getSystemProperties());
+                System.out.printf("%nApplication properties (set by device):%n%s",
+                    partitionEvent.getData().getProperties());
+                System.out.printf("%nSystem properties (set by IoT Hub):%n%s",
+                        partitionEvent.getData().getSystemProperties());
               });
             }, ex -> {
               System.out.println("Error receiving events " + ex);
