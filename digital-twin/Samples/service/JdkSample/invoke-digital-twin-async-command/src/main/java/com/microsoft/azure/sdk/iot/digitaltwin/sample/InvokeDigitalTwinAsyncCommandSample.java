fa693f1a1c3fe3d16ba20e3126f7f08a37c6fc11
hongli19750210 <v-hongli1@microsoft.com>
2020-12-29 09:27:27 +0800

    fix task262 [Sample] Azure IoT Samples for Java (EventHubs) 202012290927

--- digital-twin/Samples/service/JdkSample/invoke-digital-twin-async-command/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/sample/InvokeDigitalTwinAsyncCommandSample.java
+++ digital-twin/Samples/service/JdkSample/invoke-digital-twin-async-command/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/sample/InvokeDigitalTwinAsyncCommandSample.java
@@ -3,8 +3,10 @@
 
 package com.microsoft.azure.sdk.iot.digitaltwin.sample;
 
+import com.azure.core.util.IterableStream;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.microsoft.azure.eventhubs.*;
+import com.azure.messaging.eventhubs.*;
+import com.azure.messaging.eventhubs.models.*;
 import com.microsoft.azure.sdk.iot.digitaltwin.service.DigitalTwinServiceClient;
 import com.microsoft.azure.sdk.iot.digitaltwin.service.DigitalTwinServiceClientImpl;
 import com.microsoft.azure.sdk.iot.digitaltwin.service.models.DigitalTwinCommandResponse;
@@ -14,10 +16,9 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Iterator;
 import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.*;
 
 @Slf4j
 public class InvokeDigitalTwinAsyncCommandSample {
@@ -41,7 +42,7 @@ public class InvokeDigitalTwinAsyncCommandSample {
                     "EVENTHUB_CONNECTION_STRING - The connection string to the EventHub associated to your IoT Hub\n" +
                     "PAYLOAD - (optional) The json payload to include in the command";
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException, EventHubException, IOException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
         verifyInputs();
 
         DigitalTwinServiceClient digitalTwinServiceClient = DigitalTwinServiceClientImpl.buildFromConnectionString().connectionString(IOTHUB_CONNECTION_STRING).build();
@@ -64,80 +65,67 @@ public class InvokeDigitalTwinAsyncCommandSample {
         listenForAsyncCommandUpdates(digitalTwinCommandResponse.getRequestId());
     }
 
-    private static void listenForAsyncCommandUpdates(String requestId) throws ExecutionException, InterruptedException, EventHubException, IOException {
+    private static void listenForAsyncCommandUpdates(String requestId) throws ExecutionException, InterruptedException, IOException {
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(THREAD_POOL_MAX_NUMBER);
-        EventHubClient eventHubClient = EventHubClient.createFromConnectionStringSync(EVENTHUB_CONNECTION_STRING, executorService);
-        EventHubRuntimeInformation eventHubInfo = eventHubClient.getRuntimeInformation().get();
-        String[] partitionIds = eventHubInfo.getPartitionIds();
-
-        for (String partitionId : partitionIds) {
-            Instant lookbackInSeconds = Instant.now().minusSeconds(0);
-            PartitionReceiver partitionReceiver = eventHubClient.createReceiverSync(EventHubClient.DEFAULT_CONSUMER_GROUP_NAME, partitionId, EventPosition.fromEnqueuedTime(lookbackInSeconds));
-            partitionReceiver.setReceiveTimeout(Duration.ofSeconds(RECEIVE_TIMEOUT));
-
-            log.debug("EventHub receiver created for partition {}, listening from {} [OperationTimeout: {} secs]", partitionId, lookbackInSeconds, RECEIVE_TIMEOUT);
-            new Thread(() -> {
-                while (!executorService.isShutdown()) {
-                    try {
-                        receiveMessages(partitionReceiver, executorService, requestId);
-                        Thread.sleep(1000);
-                    }
-                    catch (InterruptedException e) {
-                        log.info("Eventhub receiver thread interrupted, gracefully closing the receiver now");
-                        try {
-                            partitionReceiver.closeSync();
-                        }
-                        catch (EventHubException ex) {
-                            log.error("Failed to close partition receiver", ex);
-                        }
-                    }
-                    catch (ExecutionException e) {
-                        log.error("Eventhub receiver thread encountered ExecutionException, gracefully closing the receiver now", e);
-                        try {
-                            partitionReceiver.closeSync();
-                        }
-                        catch (EventHubException ex) {
-                            log.error("Failed to close partition receiver", ex);
-                        }
-                    }
+        EventHubConsumerClient eventHubConsumerClient = new EventHubClientBuilder()
+                .connectionString(EVENTHUB_CONNECTION_STRING)
+                .consumerGroup(EventHubClientBuilder.DEFAULT_CONSUMER_GROUP_NAME)
+                .buildConsumerClient();
+        ConcurrentMap<String,Instant> instantMap = new ConcurrentHashMap();
+        Iterator<String> partitionIterators = eventHubConsumerClient.getPartitionIds().iterator();
+        while (partitionIterators.hasNext()){
+            String partitionId = partitionIterators.next();
+            instantMap.put(partitionId,Instant.now().minusSeconds(0));
+            executorService.scheduleWithFixedDelay(() -> {
+                IterableStream<PartitionEvent> partitionEventIterableStream =
+                        eventHubConsumerClient.receiveFromPartition(partitionId,1,
+                                EventPosition.fromEnqueuedTime(instantMap.get(partitionId)),
+                                Duration.ofSeconds(RECEIVE_TIMEOUT));
+                System.out.println(String.format("EventHub receiver created for partition %s, listening from %s [OperationTimeout: %s secs]", partitionId, instantMap.get(partitionId), RECEIVE_TIMEOUT));
+                try {
+                    receiveMessages(partitionEventIterableStream, partitionId, requestId);
+                } catch (InterruptedException e) {
+                    log.error("Eventhub receiver thread interrupted, gracefully closing the receiver now");
+                } catch (ExecutionException e) {
+                    log.error(String.format("Eventhub receiver thread encountered ExecutionException, gracefully closing the receiver now : %s", e.getMessage()));
                 }
-            }).start();
+            }, 0, 1000, TimeUnit.MILLISECONDS);
         }
 
         //Wait for user to enter a key to exit the program
         new Scanner(System.in).nextLine();
-        executorService.shutdownNow();
+        executorService.shutdown();
+        eventHubConsumerClient.close();
         System.exit(0);
     }
 
-    private static void receiveMessages(PartitionReceiver partitionReceiver, ScheduledExecutorService executorService, String requestId) throws InterruptedException, ExecutionException {
-        log.trace("Receiving from partition: {}", partitionReceiver.getPartitionId());
-        partitionReceiver.receive(1).thenAcceptAsync(eventData -> {
-            if (eventData == null) {
-                log.trace("No events received.");
-            }
-            else {
-                long batchSize = eventData.spliterator().getExactSizeIfKnown();
-                log.trace("ReceivedBatch Size: {}", batchSize);
+    private static void receiveMessages(IterableStream<PartitionEvent> partitionEventIterableStream, String partitionId, String requestId) throws InterruptedException, ExecutionException {
+        log.trace(String.format("Receiving from partition: %s", partitionId));
+        long batchSize = partitionEventIterableStream.spliterator().getExactSizeIfKnown();
+        Iterator<PartitionEvent> eventIterator = partitionEventIterableStream.iterator();
+        if (!eventIterator.hasNext()) {
+            log.trace("No events received.");
+        } else {
+            log.trace("ReceivedBatch Size: {}", batchSize);
+            while ( eventIterator.hasNext() ) {
+                EventData receivedEvent = eventIterator.next().getData();
+                if (receivedEvent.getProperties() != null
+                        && receivedEvent.getProperties().keySet().contains(COMMAND_REQUEST_ID_PROPERTY_NAME)) {
+                    String payload = new String(receivedEvent.getBody(), Charset.forName("UTF-8"));
 
-                for (EventData receivedEvent : eventData) {
-                    if (receivedEvent.getProperties() != null && receivedEvent.getProperties().keySet().contains(COMMAND_REQUEST_ID_PROPERTY_NAME)) {
-                        String payload = new String(receivedEvent.getBytes(), Charset.forName("UTF-8"));
+                    log.info("Received an update on the async command:");
+                    for (String propertyKey : receivedEvent.getProperties().keySet()) {
+                        log.info("    " + propertyKey + ":" + receivedEvent.getProperties().get(propertyKey));
+                    }
+                    log.info("    " + "Update Payload: ");
+                    log.info("        " + payload);
 
-                        log.info("Received an update on the async command:");
-                        for (String propertyKey : receivedEvent.getProperties().keySet()) {
-                            log.info("    " + propertyKey + ":" + receivedEvent.getProperties().get(propertyKey));
-                        }
-                        log.info("    " + "Update Payload: ");
-                        log.info("        " + payload);
-
-                        if (payload.contains("100%")) {
-                            log.info("Async command has finished, enter any key to finish\n");
-                        }
+                    if (payload.contains("100%")) {
+                        log.info("Async command has finished, enter any key to finish\n");
                     }
                 }
             }
-        }, executorService).get();
+        }
     }
 
     private static void verifyInputs() {
