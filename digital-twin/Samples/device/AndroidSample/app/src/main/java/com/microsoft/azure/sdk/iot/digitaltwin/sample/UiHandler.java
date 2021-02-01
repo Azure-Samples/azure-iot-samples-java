a15a005643ea50bda6a3a8c92f40fa956e867b82
timtay-microsoft <timtay@microsoft.com>
2021-02-01 10:41:42 -0800

    Remove out of date digital twin samples, link to current samples
    The digital twin samples that lived in this repo were tied to the pre-summer refresh PnP preview service. There has been a formal release of PnP that came with different SDK libraries and different samples, so these samples should be removed to avoid confusion

--- digital-twin/Samples/device/AndroidSample/app/src/main/java/com/microsoft/azure/sdk/iot/digitaltwin/sample/UiHandler.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.azure.sdk.iot.digitaltwin.sample;
-
-interface UiHandler {
-    void updateName(String name);
-    void updateBrightness(double brightness);
-    void updateTemperatureAndHumidity(double temperature, double humidity);
-    void updateOnoff(boolean on);
-    void startBlink(long interval);
-}
